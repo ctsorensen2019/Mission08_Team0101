07fa1896c3c0102f9d3e992130aa88fe6839a916
Christian Sorensen <ctsorensen2019@gmail.com>
2025-02-27 08:47:42 -0700

    commited

--- Mission08_Team0101/Controllers/HomeController.cs
+++ Mission08_Team0101/Controllers/HomeController.cs
@@ -9,13 +9,8 @@ namespace Mission08_Team0101.Controllers;
 public class HomeController : Controller
 {
 
-    private QuadrantContext _context;
-
-    public HomeController(QuadrantContext temp)//Constructor
-    {
-        _context = temp;
-
     private HabitContext _context;
+
     public HomeController(HabitContext temp) //Constructor
     {
         _context = temp; //Storing temporary variable
@@ -34,56 +29,56 @@ public class HomeController : Controller
 
 
 
-    //public IActionResult EditMovie()
-    //{
+    public IActionResult EditTask()
+    {
 
-    //    var movies = _context.Movies
-    //        .ToList();
+        var habit = _context.Habits
+            .ToList();
 
-    //    return View(movies);
-    //}
+        return View(habit);
+    }
 
-    //[HttpGet]
-    //public IActionResult Edit(int id)
-    //{
-    //    var recordToEdit = _context.Movies
-    //        .Single(x => x.MovieId == id);
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var recordToEdit = _context.Habits
+            .Single(x => x.HabitId == id);
 
 
 
-    //    ViewBag.Categories = _context.Categories
-    //        .OrderBy(x => x.CategoryName)
-    //        .ToList();
+        ViewBag.Categories = _context.Habits
+            .OrderBy(x => x.CategoryName)
+            .ToList();
 
-    //    return View("MovieForm", recordToEdit);
-    //}
+        return View("AddTask", recordToEdit);
+    }
 
-    //[HttpPost]
-    //public IActionResult Edit(Habit updatedInfo)
-    //{
-    //    _context.Update(updatedInfo);
-    //    _context.SaveChanges();
+    [HttpPost]
+    public IActionResult Edit(Habit updatedInfo)
+    {
+        _context.Update(updatedInfo);
+        _context.SaveChanges();
 
-    //    return RedirectToAction("EditTask");
-    //}
+        return RedirectToAction("EditTask");
+    }
 
-    //[HttpGet]
-    //public IActionResult Delete(int id)
-    //{
-    //    var recordToDelete = _context.Movies
-    //        .Single(x => x.MovieId == id);
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var recordToDelete = _context.Movies
+            .Single(x => x.HabitId == id);
 
-    //    return View(recordToDelete);
-    //}
+        return View(recordToDelete);
+    }
 
-    //[HttpPost]
-    //public IActionResult Delete(Habit application)
-    //{
-    //    _context.Movies.Remove(application);
-    //    _context.SaveChanges();
+    [HttpPost]
+    public IActionResult Delete(Habit application)
+    {
+        _context.Habits.Remove(application);
+        _context.SaveChanges();
 
-    //    return RedirectToAction("EditMovie");
-    //}
+        return RedirectToAction("EditMovie");
+    }
 
     [HttpGet]
     public IActionResult AddTask()
